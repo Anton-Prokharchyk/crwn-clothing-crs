bda1b6d1cdf74cc8b377dc1b61470411159fe3b8
Anton.Prokharchyk <Anton.Prokharchyk@gmail.com>
2025-12-07 17:37:01 +0300

    initial react vire app

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        <img src="" />
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Show Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <img src="" />
+        <div className="category-body-container">
+          <h2>Jackers</h2>
+          <p>Show Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <img src="" />
+        <div className="category-body-container">
+          <h2>Sneakers</h2>
+          <p>Show Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <img src="" />
+        <div className="category-body-container">
+          <h2>womens</h2>
+          <p>Show Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <img src="" />
+        <div className="category-body-container">
+          <h2>mens</h2>
+          <p>Show Now</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
